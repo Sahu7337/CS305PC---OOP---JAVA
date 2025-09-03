f64cd5e89d57eb90ef9aacb332ac573c6f394e2c
AsishRanjan7337_ <asishranjansahoo@outlook.com>
2025-09-03 09:56:10 +0530

    Add files via upload

---
+++ Soul.java
@@ -0,0 +1,15 @@
+package Java;
+
+class Soul {
+    public static void main(String[] args) {
+        Soul so = new Soul();
+        so.display();
+        
+        System.out.println("I'll save you");
+    }
+    
+    public void display() {
+        System.out.println("No one is coming to save you");
+        //System.exit(0);
+    }
+}
