fc1f59a22a051dd882b466767d031dfbaa25a3b5
AsishRanjan7337_ <asishranjansahoo@outlook.com>
2025-09-10 11:09:39 +0530

    Create Lab2P2.java

---
+++ LAB2/Lab2P2.java
@@ -0,0 +1,11 @@
+
+public class Lab2P2 {
+    public static void main(String[] args) {
+        int a = 5;
+
+        System.out.println("The Output is: " + (a++ + ++a));
+        //System.out.println(a++ + ++a + "The Output is: ");
+        //System.out.println("The Output is: " + a++ + ++a);
+        //System.out.println(a++ + ++a);
+    }
+}
