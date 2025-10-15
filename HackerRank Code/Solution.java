6fd5f788e536150d4f68eb696da5dca506a5b20b
AsishRanjan7337_ <asishranjansahoo@outlook.com>
2025-10-15 10:26:10 +0530

    Create Solution.java

---
+++ HackerRank Code/Solution.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        try(Scanner sc = new Scanner(System.in)){
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            
+            System.out.println(x/y);
+        } catch(InputMismatchException ime) {
+            System.out.println(ime.getClass().getName());
+        } 
+         catch(ArithmeticException ae) {
+            System.out.println(ae.getClass().getName() + ": " + ae.getMessage());
+        } 
+    }
+}
